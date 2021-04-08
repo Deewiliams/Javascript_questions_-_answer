9ba37e6ca015183bea64cb91c521dd1cad9b84b7
Your Name <desireirankundawilliams@gmail.com>
2021-04-08 14:30:56 +0200

    Write a JavaScript function to check whether a string is blank or not.

---
+++ index.js
@@ -0,0 +1,8 @@
+//Write a JavaScript function to check whether a string is blank or not.
+
+function is_Blank(blank) {
+    return !blank.length;
+}
+
+console.log(is_Blank(''));
+console.log(is_Blank('abc'));

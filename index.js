20ebfd3c5bee10a364db04600a2167b3ffcc7d4e
Your Name <desireirankundawilliams@gmail.com>
2021-04-11 23:37:03 +0200

    modified a question, Write a JavaScript function to check whether a string is blank or not.

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 //Write a JavaScript function to check whether a string is blank or not.
 
-function is_Blank(blank) {
-    return !blank.length;
+function is_Blank (blank){
+    if (blank.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
+console.log(is_Blank (''));
+console.log(is_Blank ('abc'));
 
-console.log(is_Blank(''));
-console.log(is_Blank('abc'));
 
 
 
 //Write a JavaScript function to split a string and convert it into an array of words.
 
-let senstence = "my name is Desire";
-let words = sentence.split('');
-console.log('words')
+function sentence (str) {
+    return str.split(" ");
+
+}
+
+console.log(sentence('my name is Desire'));
 
 //Write a JavaScript function to remove specified number of characters from a string.
 

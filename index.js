e464f718c3284b730e0f75a7db7eb9274fc657c9
Your Name <desireirankundawilliams@gmail.com>
2021-04-15 20:13:12 +0200

    modified the bigger number question

--- index.js
+++ index.js
@@ -82,11 +82,20 @@ function truncate_string (str1, length) {
 
       //Write a function named biggerNumber that takes 2 arguments, both numbers returns whichever number is the greater (higher) number, so if you give 2, 3 then it should 3. 
       
-      function biggerNumber(num) {
-        if (num < 2 && num > 3) {
-            return "2";
+      function biggerNumber(num, num1) {
+        if(num === num1) {
+            return parseInt(num)
         }
-        return "3"
-    }
-     
-    console.log(biggerNumber(2,3));
+        else if (num >= num1 ) {
+          return parseInt(num );
+        }
+        else if (num <= num1){
+            return parseInt( num1);
+        }
+      }
+    
+      
+      console.log(biggerNumber(2, 3));
+      console.log(biggerNumber(12, 3));
+      console.log(biggerNumber(0, -3));
+      console.log(biggerNumber(1, 1));
